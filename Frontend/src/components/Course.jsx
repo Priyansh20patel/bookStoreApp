fd6598280dea1107254ec5680cd09a620d9d17ef
Priyansh <priyashpatel21@gnu.ac.in>
2024-06-28 18:14:13 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,89 @@
+// import React, { useEffect, useState } from "react";
+// import Cards from "./Cards";
+// import axios from "axios";
+// import { Link } from "react-router-dom";
+// function Course() {
+//   const [book, setBook] = useState([]);
+//   useEffect(() => {
+//     const getBook = async () => {
+//       try {
+//         const res = await axios.get("http://localhost:4001/book");
+//         console.log(res.data);
+//         setBook(res.data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     getBook();
+//   }, []);
+//   return (
+//     <>
+//       <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+//         <div className="mt-28 items-center justify-center text-center">
+//           <h1 className="text-2xl  md:text-4xl">
+//             We're delighted to have you{" "}
+//             <span className="text-pink-500"> Here! :)</span>
+//           </h1>
+//           <p className="mt-12">
+
+//           </p>
+//           <Link to="/">
+//             <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+//               Back
+//             </button>
+//           </Link>
+//         </div>
+//         <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+//           {book.map((item) => (
+//             <Cards key={item.id} item={item} />
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Course;
+
+
+import React from 'react';
+import Cards from "./Cards";
+import list from "../../public/list.json";
+import { Link } from "react-router-dom";
+
+function Course() {
+  return (
+    <>
+      <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
+        <div className="mt-28 items-center justify-center text-center">
+
+          <h1 className="text-2xl  md:text-4xl">   We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+            et totam. Tempora amet atque expedita, quae corrupti totam sed
+            pariatur corporis at veniam est voluptas animi!
+          </p>
+
+
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {
+            list.map((item) => (
+              <Cards key={item.id} item={item} />
+            ))
+          }
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+export default Course
